72e088e5590b3d8961eb147ce645756ad28c3499
Jaylan Zhao <natarichjay@gmail.com>
2021-04-04 15:56:53 +0800

    Rearrange

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./assets/ARCADECLASSIC.TTF":[["ARCADECLASSIC.0b7f7d82.TTF","assets/ARCADECLASSIC.TTF"],"assets/ARCADECLASSIC.TTF"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./assets\\ARCADECLASSIC.TTF":[["ARCADECLASSIC.0b7f7d82.TTF","assets/ARCADECLASSIC.TTF"],"assets/ARCADECLASSIC.TTF"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52254" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60646" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
